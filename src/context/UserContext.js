5ad86ad8fa075dd2763e6851ed311d90181b4478
Miguel Santos <miguelsantosf12@gmail.com>
2024-06-14 17:48:19 -0300

    remove: removido useUser

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,19 +1,15 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { createContext } from "react"
 
 import useAuth from "../hooks/useAuth"
-import useUser from "../hooks/useUser"
 import usePay from "../hooks/usePay"
-import api from "../utils/api";
-import { useNavigate } from "react-router-dom";
 
 const Context = createContext()
 
 function UserProvider({ children }) {
-  const { editUser } = useUser()
   const { authenticated, register, login, logout, forgotpassword } = useAuth()
   const { registerKey, checkReceiver, confirmPayment, makePayment } = usePay()
 
-  return <Context.Provider value={{ authenticated, register, login, logout, forgotpassword, editUser, registerKey, checkReceiver, confirmPayment, makePayment }}>{children}</Context.Provider>
+  return <Context.Provider value={{ authenticated, register, login, logout, forgotpassword, registerKey, checkReceiver, confirmPayment, makePayment }}>{children}</Context.Provider>
 
 }
 
